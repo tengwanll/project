aba90c73c5d119ca02255b5f49a14cb33a1e8391
zhengyue <zhengyue@mirror-networks.com>
2016-04-10 11:21:37 +0800

    暂时用mysq

--- Web/Common/Conf/config.php
+++ Web/Common/Conf/config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'DB_TYPE'   => 'pdo', // 数据库类型
+	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => '123456', // 密码
-	'DB_DSN'    => 'mysql:host=localhost;dbname=project;charset=UTF8;port=3306',
-	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_DSN'    => 'mysql:host=120.27.5.26;dbname=project;charset=UTF8;port=3306',
+	'DB_HOST'   => '120.27.5.26', // 服务器地址
 	'DB_PREFIX' => '', // 数据库表前缀 
 	'DEFAULT_FILTER'=>'',
 	'SHOW_PAGE_TRACE' =>false,//开启thinkPHP调试模式
