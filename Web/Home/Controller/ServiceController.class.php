2aa1343cfe4a865ff128dc967eede880fbb72c1e
zhengyue <zhengyue@mirror-networks.com>
2016-04-26 22:15:44 +0800

    图片调整

--- Web/Home/Controller/ServiceController.class.php
+++ Web/Home/Controller/ServiceController.class.php
@@ -24,7 +24,7 @@ class ServiceController extends Controller {
         $this->display();
     }
 
-    public function list(){
+    public function lists(){
         $serviceId=I('get.serviceId');
         $service=M('service_sort');
         $serviceInfo=M('service');
