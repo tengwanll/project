d4237f78722388cb0b5a23265be84d0be9dfe1e4
honpery <honpery@gmail.com>
2016-05-07 14:13:16 +0800

    前台界面调整

--- Public/Home/public/js/public.js
+++ Public/Home/public/js/public.js
@@ -3,27 +3,30 @@ $(document).ready(function() {
 
 
 
-	/**
-	 * header nav 展示
-	 */
-	var navTime = null;
-	$(document).on('mouseover', '.header nav > ul > li > a', function () {
-		$(this).next().slideDown(80);
-	});
+    /**
+     * header nav 展示
+     */
+    var navTime = null;
+    $(document).on('mouseover', '.header nav > ul > li > a', function() {
+        $(this).next().slideDown(80);
+    });
 
-	$(document).on('mouseout', '.header nav > ul > li > a', function () {
-		var list = $(this).next();
-		navTime = setTimeout(function () {
-			list.slideUp(80);
-		}, 10);
-	});
+    $(document).on('mouseout', '.header nav > ul > li > a', function() {
+        var list = $(this).next();
+        navTime = setTimeout(function() {
+            list.slideUp(80);
+        }, 10);
+    });
 
-	$(document).on('mouseover', '.header nav > ul ul', function () {
-		clearTimeout(navTime);
-		$(this).show();
-	});
+    $(document).on('mouseover', '.header nav > ul ul', function() {
+        clearTimeout(navTime);
+        $(this).show();
+    });
 
-	$(document).on('mouseout', '.header nav > ul > li > ul', function () {
-		$(this).hide();
-	});
+    $(document).on('mouseout', '.header nav > ul > li > ul', function() {
+        $(this).hide();
+    });
+
+
+
 });
