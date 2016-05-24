6dbdd829147dc03c82e8b3208edaf1a82d94acf7
zhengyue <zhengyue@mirror-networks.com>
2016-05-24 12:07:00 +0800

    check

--- Public/Home/message/js/message.js
+++ Public/Home/message/js/message.js
@@ -15,10 +15,26 @@ $(document).ready(function() {
 
 		// check
 		for(var i in postDatas) {
+            var telephone=/^((13[0-9])|(15[0-9])|(18[0-9])|(17[0-9]))\d{8}$/;
+            var email=/^[_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,4}$/;
+            var phone=/^((\(\d{3}\))|(\d{3}\-))?(\(0\d{2,3}\)|0\d{2,3}-)?[1-9]\d{6,7}$/;
 			if (!postDatas[i]) {
 				alert($('input[name="' + i + '"]').prev().html() + '不能为空。');
 				return;
-			}
+			}else{
+                if(!telephone.exec($('#telephone').val())){
+                    alert( '手机号码格式不对。');
+                    return;
+                }
+                if(!email.exec($('#email').val())){
+                    alert('邮箱格式不对。');
+                    return;
+                }
+                if(!phone.exec($('#tel').val())){
+                    alert('固定电话格式不对。');
+                    return;
+                }
+            }
 		}
 
 		var btn = $(this);
