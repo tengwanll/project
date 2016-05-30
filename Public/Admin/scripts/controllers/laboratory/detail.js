cc4a261ece6238b84b7f494bd6f2da0d4d4e9495
honpery <honpery@gmail.com>
2016-05-30 13:29:09 +0800

    图片集功能实现

--- Public/Admin/scripts/controllers/laboratory/detail.js
+++ Public/Admin/scripts/controllers/laboratory/detail.js
@@ -6,7 +6,7 @@ define(['app'], function(app) {
                     { title: '名称', type: 'input', key: 'name' },
                     { title: '封面', type: 'photo', key: 'photo' },
                     { title: '简介', type: 'text', key: 'description' },
-                    { title: '图片集', type: 'photoset', key: 'photoDetailUrl' },
+                    { title: '图片集', type: 'photoset', key: 'photoDetail' },
                 ],
                 api: {
                     add: '/Admin/laboratory/create',
