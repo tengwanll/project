a587947e23d7f842fb3034abb47037da895e99ec
zhengyue <zhengyue@mirror-networks.com>
2016-05-30 18:09:39 +0800

    图片集

--- Web/Home/Controller/LaboratoryController.class.php
+++ Web/Home/Controller/LaboratoryController.class.php
@@ -23,4 +23,48 @@ class LaboratoryController extends Controller {
         $this->assign('labs',$labs);
         $this->display();
     }
+
+    public function lists(){
+        $page=I('get.page')?I('get.page'):1;
+        $rows=I('get.rows')?I('get.rows'):9;
+        $pagePart=$page.','.$rows;
+        $labModel=M('lab');
+        $fileModel=M('file');
+        $where='status=1';
+        $labList=$labModel->where($where)->page($pagePart)->select();
+        $total=$labModel->where($where)->count();
+        $arr=array();
+        foreach($labList as $lists){
+            $photoId=$lists['photo'];
+            $photo=$fileModel->where("id=$photoId")->find();
+            $photoUrl=$photo?__ROOT__.'/'.$photo['url']:'';
+            $photoDetailUrl=array();
+            $labPhotos=json_decode($lists['photo_detail'],true);
+            foreach($labPhotos as $labPhoto){
+                $photoDetail=$fileModel->where("id=$labPhoto")->find();
+                $photoDetailUrl[]=$photoDetail?__ROOT__.'/'.$photoDetail['url']:'';
+            }
+            $arr[]=array(
+                'id'=>$lists['id'],
+                'name'=>$lists['name'],
+                'photoDetailUrl'=>$photoDetailUrl,
+                'description'=>$lists['description'],
+                'photo'=>$photoUrl,
+                'createTime'=>$lists['create_time']
+            );
+        }
+        $result=array(
+            'list'=>$arr,
+            'total'=>$total,
+            'page'=>$page
+        );
+        $response=array(
+            'errno'=>0,
+            'ermsg'=>'',
+        );
+        if($result){
+            $response['result']=$result;
+        }
+        $this->ajaxReturn($response);
+    }
 }
