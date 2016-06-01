fdf72d965daf36c717dca11f26f1819a61083d7d
honpery <honpery@gmail.com>
2016-06-01 23:04:55 +0800

    bug fixed

--- Public/Admin/scripts/controllers/service/detail.js
+++ Public/Admin/scripts/controllers/service/detail.js
@@ -7,13 +7,19 @@ define(['app'], function(app) {
                     { title: '分类', type: 'select', key: 'sort_id'},
                     { title: '概述', type: 'text', key: 'shortDesc'},
                     { title: '项目简介', type: 'editor', key: 'description'},
+
                     { title: '实验流程', type: 'editor', key: 'experimentFlow', sort: 1 },
                     { title: '用户需知', type: 'editor', key: 'userNotice', sort: 1 },
                     { title: '结果展示', type: 'editor', key: 'resultShow', sort: 1 },
                     { title: '服务周期', type: 'editor', key: 'serverCircle', sort: 1 },
-                    { title: '实验原理', type: 'editor', key: 'experiment_theory', sort: 2 },
+
+                    { title: '实验原理', type: 'editor', key: 'experimentTheory', sort: 2 },
                     { title: '项目优势', type: 'editor', key: 'advantage', sort: 2 },
-                    { title: '实验流程', type: 'editor', key: 'experiment_flow', sort: 2 },
+                    { title: '实验流程', type: 'editor', key: 'experimentFlow', sort: 2 },
+                    { title: '结果展示', type: 'editor', key: 'resultShow', sort: 2 },
+                    { title: '服务周期', type: 'editor', key: 'serverCircle', sort: 2 },
+                    { title: '参考文献', type: 'editor', key: 'experimentFlow', sort: 2 },
+                    { title: '客户须知', type: 'editor', key: 'userNotice', sort: 2 },
                 ],
                 api: {
                     add: '/Admin/service/create',
