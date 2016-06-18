b2459f79dcc3fa0e58505238470710857ecf1238
honpery <honpery@gmail.com>
2016-06-18 22:51:47 +0800

    bug fixed

--- Public/Home/laboratory/js/laboratory.js
+++ Public/Home/laboratory/js/laboratory.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    var getDatas = false;
-    var hasMore = true;
+    var gettingDatas = false;
+    var hasMoreDatas = true;
     var oLoad = $('.loading');
-    var currentPage = 0;
+    var currentPage = 1;
     var currentPhotoIndex = 0;
     var currentData = {};
     var rows = 12;
@@ -13,29 +13,19 @@ $(document).ready(function() {
     var colRight = $(oList.find('ul')[2]);
 
     // 获取第一页数据
-    getListDatas();
+    getListDatas(currentPage);
 
     // 动态加载
     $(document).on('scroll', function(e) {
-        // console.log(oLoad.position());
-        // console.log(oLoad.offset());
-        // console.log($(this).scrollTop());
-
-        if ($(this).scrollTop() > oList.height() / 2 && !getDatas) {
-            console.log(1)
-            getDatas = true;
-            console.log(hasMore);
-            if (hasMore) {
-                getListDatas(++currentPage, function(data) {
-                    if (!hasMore) {
-                        oLoad.hide();
-                        return;
-                    }
-                    var indexBegin = currentPage * rows;
-
-                    // 插入数据
-                    for (var i = 0, len = data.length; i < len; i++) {
-                        var html = '                                                \
+        if ($(this).scrollTop() > oList.height() / 2 && !gettingDatas && hasMoreDatas) {
+            hasMoreDatas = false;
+            gettingDatas = true;
+            getListDatas(++currentPage, function(data) {
+                console.log(data)
+                var indexBegin = (currentPage - 1) * rows;
+                // 插入数据
+                for (var i = 0, len = data.length; i < len; i++) {
+                    var html = '                                                \
                             <li data-listindex="' + (indexBegin + i) + '">          \
                                 <a href="#">                                        \
                                     <img src="' + data[i].photo + '">               \
@@ -45,17 +35,17 @@ $(document).ready(function() {
                                 </a>                                                \
                             </li>                                                   \
                         ';
-                        colLeft.append(html);
-                        var leftHeight = $(oList.find('ul')[0]).height();
-                        var centerHeight = $(oList.find('ul')[1]).height();
-                        var rightHeight = $(oList.find('ul')[2]).height();
-                        var heightArr = [leftHeight, centerHeight, rightHeight].sort();
-                        (leftHeight === heightArr[0] ? colLeft : (centerHeight === heightArr[0] ? colCenter : colRight)).append(html);
-                    }
-
-                    getDatas = false;
-                });
-            }
+                    var leftHeight = $(oList.find('ul')[0]).height();
+                    var centerHeight = $(oList.find('ul')[1]).height();
+                    var rightHeight = $(oList.find('ul')[2]).height();
+                    var heightArr = [leftHeight, centerHeight, rightHeight].sort();
+                    (leftHeight === heightArr[0] ? colLeft : (centerHeight === heightArr[0] ? colCenter : colRight)).append(html);
+                }
+                gettingDatas = false;
+                if (!hasMoreDatas) {
+                    $('.loading').hide();
+                }
+            });
         }
     });
 
@@ -113,22 +103,21 @@ $(document).ready(function() {
         $('.detail').hide();
     });
 
+
+    // 获取数据
     function getListDatas(page, cb) {
-        console.log(arguments);
         var options = {
-            page: page || 0,
+            page: page,
             rows: rows
         };
 
         $.get({
             url: '/Home/laboratory/lists/rows/' + options.rows + '/page/' + options.page,
             success: function(data) {
-                if (data.result.page * options.rows + data.result.list.length >= parseInt(data.result.total)) {
-                    hasMore = false;
-                } else {
-                    hasMore = true;
-                }
                 listDatas = listDatas.concat(data.result.list);
+                if (listDatas.length === data.result.total) {
+                    hasMoreDatas = false;
+                }
                 if (cb) cb(data.result.list);
             },
             error: function() {
@@ -136,7 +125,4 @@ $(document).ready(function() {
             }
         });
     }
-
-
-
 });
