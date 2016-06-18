23236b356906250379e28d896777d145ca7cfdc4
honpery <honpery@gmail.com>
2016-06-19 00:03:22 +0800

    bug fixed

--- Public/Home/public/js/public.js
+++ Public/Home/public/js/public.js
@@ -1,36 +1,17 @@
 $(document).ready(function() {
 
 
-
-
     /**
      * header nav 展示
      */
-    var navTime = null;
-    $(document).on('mouseover', '.header nav > ul > li > a', function() {
-        $(this).next().slideDown(80);
-        $(this).addClass('hover');
+
+    $(document).on('click', '.header nav > ul > li > a', function(e) {
+        if ($(this).next()) {
+            $(this).parent().parent().find('.hover').removeClass('hover')
+                .find('ul').slideUp(80);
+            $(this).parent().addClass('hover')
+                .find('ul').slideDown(80);
+        }
     });
-
-    $(document).on('mouseout', '.header nav > ul > li > a', function() {
-        var _this = $(this);
-        var list = _this.next();
-        navTime = setTimeout(function() {
-            list.slideUp(80);
-            _this.removeClass('hover');
-        }, 10);
-    });
-
-    $(document).on('mouseover', '.header nav > ul ul', function() {
-        clearTimeout(navTime);
-        $(this).show();
-    });
-
-    $(document).on('mouseout', '.header nav > ul ul', function() {
-        $(this).hide();
-        $('.header nav .hover').removeClass('hover');
-    });
-
-
 
 });
