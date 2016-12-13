f2b99c906bf795b1b832304e58ae5ef3aa24be9c
zhengyue <zhengyue@minshengwuyou.com>
2016-12-13 21:27:09 +0800

    标题关键字

--- Web/Common/Conf/config.php
+++ Web/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'   => 'pdo', // 数据库类型
 	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '123456', // 密码
+	'DB_PWD'    => 'BAIhao@database201404', // 密码
 	'DB_DSN'    => 'mysql:host=120.27.5.26;dbname=project;charset=UTF8;port=3306',
 	'DB_HOST'   => '120.27.5.26', // 服务器地址
 	'DB_PREFIX' => '', // 数据库表前缀 
